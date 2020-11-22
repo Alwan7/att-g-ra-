cc403cf26774006d2aa9479d482a3f534c1fa818
Alwan7 <70641931+Alwan7@users.noreply.github.com>
2020-11-22 17:23:39 +0100

    calculation is done

---
+++ app.js
@@ -0,0 +1,39 @@
+document.getElementById('loan-form').addEventListener('submit', calculateres);
+//räkna
+function calculateres(e) {
+    const amount = document.querySelector('#amount');
+    const intrest = document.querySelector('#interest');
+    const years = document.querySelector('#years');
+    const monthPayment = document.querySelector('#monthly-payment');
+    const totalPayment = document.querySelector('#total-payment');
+    const totintrest = document.querySelector('#total-interest');
+
+    const amValue = parseFloat(amount.value);
+    const calcIntrest = parseFloat(intrest.value) / 100 / 12;
+    const calcPayment = parseFloat(years.value) * 12;
+
+    // räkna månad betalning
+    const z = Math.pow(1 + calcIntrest, calcPayment);
+    const monthly = (amValue * z * calcIntrest) / (z - 1);
+    if (isFinite(monthly)) {
+        monthPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calcPayment).toFixed(2)
+        totintrest.value = ((monthly * calcPayment) - amValue).toFixed(2);
+
+    } else {
+        displayError('något gick fel');
+    }
+
+    e.preventDefault();
+}
+
+function displayError(error) {
+
+    errorDiv = document.createElement('div');
+    errorDiv.className = 'alert alert-danger';
+    errorDiv.appendChild(document.createTextNode(error));
+    const heading = document.querySelector('.heading');
+    const card = document.querySelector('.card');
+    card.insertBefore(errorDiv, heading);
+
+}
